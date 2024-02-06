e8c5572ac6de65a7e5fdb701619885129893d388
bits-shreyash <shreyashwadgaonkar@benchmarkit.solutions>
2024-02-06 16:09:03 +0530

    inital

---
+++ test.js
@@ -0,0 +1,5 @@
+const data = {"data":[
+{"id":1001,"first_name":"Alice","last_name":"brown","dob":"04/04/1990"},
+{"id":1002,"first_name":"Cherry","last_name":"kolhe","dob":"23/02/1980"},
+{"id":1003,"first_name":"Nicole","last_name":"kidman","dob":"03/11/1985"}
+],"total_records":3,"per_page":1,"current_page":1}
